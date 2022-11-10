78e654ae4045b8959a5718f78e672c8505d21efe
AudranBert <audran.bert@alumni.univ-avignon.fr>
2022-11-10 15:08:21 +0100

    add: load

---
+++ load.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import json
+
+
+def compute_basics_on_file(title, df):
+    title = " "+title+" "
+    print(f'{title.upper().center(50, "-")}')
+    print(f"Nombre de commentaires: {len(df)}")
+    # mean = df["note"].mean()
+    #print(f'{mean}')
+
+
+file = "dataset/dev.xml"
+df_dev = pd.read_xml(file)
+
+# json_result = df.to_json()
+# out = open("test.json", "w")
+# json.dump(result, "test.csv")
+
+# file = "dataset/train.xml"
+# df_train = pd.read_xml(file)
+
+# file = "dataset/test.xml"
+# df_test = pd.read_xml(file)
+
+print(df_dev.keys())
+print(df_dev)
+# compute_basics_on_file("train",df_train)
+compute_basics_on_file("dev", df_dev)
+# compute_basics_on_file("test",df_test)
