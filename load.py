40892e7d8f81ece0c48bbc33b01599858e5b6103
AudranBert <audran.bert@alumni.univ-avignon.fr>
2022-11-10 16:06:59 +0100

    add: basics metrics

--- load.py
+++ load.py
@@ -2,16 +2,24 @@ import pandas as pd
 import json
 
 
-def compute_basics_on_file(title, df):
-    title = " "+title+" "
-    print(f'{title.upper().center(50, "-")}')
+def compute_basics_analysis_df(title, df):
+    title_str = " "+title+" "
+    print(f'{title_str.upper().center(50, "-")}')
     print(f"Nombre de commentaires: {len(df)}")
-    # mean = df["note"].mean()
-    #print(f'{mean}')
+    if title != "test":
+        print(f'Moyenne des notes = {df["note"].mean():.3f}')
+        print(f'Ecart type = {df["note"].std():.3f}')
+    counts = df['movie'].value_counts()
+    print(f'Moyenne du nombre de commentaires par film = {counts.mean():.3f}')
+    print(f"Ecart type = {counts.std():.3f}")
+    counts = df['user_id'].value_counts()
+    print(f'Moyenne du nombre de commentaires par user = {counts.mean():.3f}')
+    print(f"Ecart type = {counts.std():.3f}")
+
 
 def make_float(v):
     try:
-        v=v.replace(",",".")
+        v = v.replace(",", ".")
         return float(v)
     except:
         return pd.np.nan
@@ -27,6 +35,8 @@ df_dev = load_xml("dataset/dev.xml")
 print(df_dev.keys())
 print(df_dev.dtypes)
 print(df_dev)
-# compute_basics_on_file("train",df_train)
-compute_basics_on_file("dev", df_dev)
-# compute_basics_on_file("test",df_test)
+compute_basics_analysis_df("dev", df_dev)
+# df_train = load_xml("dataset/train.xml")
+# compute_basics_analysis_df("train", df_train)
+df_test = load_xml("dataset/test.xml")
+# compute_basics_analysis_df("test", df_test)
