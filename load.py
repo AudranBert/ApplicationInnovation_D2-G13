73e69e06abe2f7912d9779f3de6019ba189c7d4e
Raphael Rey <raphael.rey@alumni.univ-avignon.fr>
2022-11-10 15:13:43 +0100

    ajout code conversion en float des notes

--- load.py
+++ load.py
@@ -9,10 +9,16 @@ def compute_basics_on_file(title, df):
     # mean = df["note"].mean()
     #print(f'{mean}')
 
+def make_float(v):
+    try:
+        return float(v)
+    except:
+        return pd.np.nan
 
 file = "dataset/dev.xml"
 df_dev = pd.read_xml(file)
 
+df_dev["note"] = df_dev["note"].apply(make_float)
 # json_result = df.to_json()
 # out = open("test.json", "w")
 # json.dump(result, "test.csv")
@@ -24,6 +30,7 @@ df_dev = pd.read_xml(file)
 # df_test = pd.read_xml(file)
 
 print(df_dev.keys())
+print(df_dev.dtypes)
 print(df_dev)
 # compute_basics_on_file("train",df_train)
 compute_basics_on_file("dev", df_dev)
